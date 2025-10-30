67200456839c609817ac7cd77f7d8fb0aa646b4b
aishwaryabetwar9 <aishwaryabetwar9@gmail.com>
2025-10-30 10:14:11 +0530

    aishwaryabetwar: create task in sql

---
+++ task.sql
@@ -0,0 +1,11 @@
+select * from sales;
+
+select * from sales order by order_date
+
+select product_id,qty from sales order by product_id , qty
+
+-- # task how to do asc product_id and desc qty
+
+SELECT product_id,qty from sales order by product_id ASC,qty DESC;
+   
+   
